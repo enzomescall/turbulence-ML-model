62e2311491f3cb65d21a249aaa9219aace7d1bd8
enzo <enzo@pop-os.localdomain>
2022-10-31 18:21:19 -0400

    made the cse function

--- enzo_model_experimentation.R
+++ enzo_model_experimentation.R
@@ -9,12 +9,6 @@ clean = data.train %>%
   mutate(Fr = as.factor(Fr),
          Re = as.factor(Re))
 
-# final models
-model_m1 = glm(log(R_moment_1) ~ Re + log(St) + Fr*St, data = clean)
-model_m2 = lm(log(R_moment_2) ~ Re + Fr + log(St) + St*Re*Fr, data = clean)
-model_m3 = lm(log(R_moment_3) ~ Re + Fr + log(St) + St*Re*Fr, data = clean)
-model_m4 = lm(log(R_moment_4) ~ Re + Fr + log(St) + St*Re*Fr, data = clean)
-
 cv_mse = function(input_model, k = 5, iter = 25, data = clean) {
   sum_mse = 0
   for(i in 1:iter) {
@@ -23,6 +17,28 @@ cv_mse = function(input_model, k = 5, iter = 25, data = clean) {
   avg_mse = sum_mse/iter
   return(avg_mse)
 }
+
+
+
+
+# experimenting for model 1
+poly_mse = c()
+for (degree in 1:10) {
+  degree_i = glm(log(R_moment_1) ~ Re + Fr + poly(St, degree) + log(St), data = clean)
+  poly_mse[degree] = cv_mse(degree_i)
+}
+min(poly_mse)
+which.min(poly_mse)
+
+spline_mse = c()
+for (df in 1:10) {
+  degree_i = glm(log(R_moment_1) ~ Re + Fr + bs(St, df = df, degree = 1), data = clean)
+  spline_mse[df] = cv_mse(degree_i)
+}
+min(spline_mse)
+which.min(spline_mse)
+
+
 
 predictive_model = function(test_St, test_Re, test_Fr) {
   newdata = data.frame(list(St = test_St, Re = as.factor(test_Re), Fr = as.factor(test_Fr)))
