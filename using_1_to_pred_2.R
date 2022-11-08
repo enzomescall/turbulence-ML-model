a7106d5a52a9113d12dbb4631d6058a44ea3b4c4
enzo <enzo@pop-os.localdomain>
2022-11-07 20:57:09 -0500

    finished predicted.test

--- using_1_to_pred_2.R
+++ using_1_to_pred_2.R
@@ -5,6 +5,10 @@ library(boot)
 data.train = read.csv("data-train.csv")
 data.test = read.csv("data-test.csv") 
 
+predicted.test = data.test  %>%
+  mutate(Fr = as.factor(Fr),
+         Re = as.factor(Re))
+
 clean = data.train %>%
   mutate(Fr = as.factor(Fr),
          Re = as.factor(Re))
@@ -13,23 +17,24 @@ lm1 = glm(R_moment_1 ~ log(St)*Fr*Re + St*Fr*Re, data = clean)
 cv.glm(data = clean, glmfit = lm1)$delta[1]
 
 predictions = clean
-predictions$pred_m1 = predict(lm1, predictions)
+predictions$moment_1 = predict(lm1, predictions)
 
-lm2 = glm(R_moment_2 ~ abs(log(St)*Fr*Re + St*Fr*Re), data = clean)
+lm2 = glm(R_moment_2 ~ log(St)*Fr*Re + St*Fr*Re, data = clean)
 cv.glm(data = clean, glmfit = lm2)$delta[1]
 
 predictions$pred_m2 = predict(lm2, predictions)
 
-lm3 = glm(R_moment_3 ~ pred_m1*Fr*Re, data = predictions)
+lm3 = glm(R_moment_3 ~ moment_1*Fr*Re, data = predictions)
 cv.glm(data = predictions, glmfit = lm3)$delta[1]
 
 predictions$pred_m3 = predict(lm3, predictions)
 
-lm4 = glm(log(R_moment_4) ~ log(St)*Fr*Re, data = predictions)
+lm4 = glm(R_moment_4 ~ log(St)*Fr*Re, data = predictions)
 cv.glm(data = predictions, glmfit = lm4)$delta[1]
 
-predictions$pred_m4 = exp(predict(lm4, predictions))
+predictions$pred_m4 = predict(lm4, predictions)
 
-predictions$diff2 = abs(predictions$R_moment_4 - predictions$pred_m4)
-
-plot(lm3)
+predicted.test$moment_1 = abs(predict(lm1, predicted.test))
+predicted.test$moment_2 = abs(predict(lm2, predicted.test))
+predicted.test$moment_3 = abs(predict(lm3, predicted.test))
+predicted.test$moment_4 = abs(predict(lm4, predicted.test))
