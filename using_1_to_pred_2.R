8af935865ab45108bc61e6d52298e46c71cc8d1c
enzo <enzo@pop-os.localdomain>
2022-11-07 19:38:13 -0500

    added pred for 1 and 2

--- using_1_to_pred_2.R
+++ using_1_to_pred_2.R
@@ -9,9 +9,14 @@ clean = data.train %>%
   mutate(Fr = as.factor(Fr),
          Re = as.factor(Re))
 
-lm = glm(R_moment_1 ~ log(St)*Fr*Re + St*Fr*Re, data = clean)
-cv.glm(data = clean, glmfit = lm)$delta[1]
+lm1 = glm(R_moment_1 ~ log(St)*Fr*Re + St*Fr*Re, data = clean)
+cv.glm(data = clean, glmfit = lm1)$delta[1]
 
 predictions = clean
-predictions$pred_m1 = predict(lm, only_predictors)
+predictions$pred_m1 = predict(lm1, predictions)
+
+lm2 = glm(R_moment_2 ~ log(St)*Fr*Re + St*Fr*Re, data = clean)
+cv.glm(data = clean, glmfit = lm2)$delta[1]
+
+predictions$pred_m2 = predict(lm2, predictions)
 
