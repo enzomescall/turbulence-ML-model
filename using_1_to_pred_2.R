6f641208b691596af8e774144eb83a40605cb94e
enzo <enzo@pop-os.localdomain>
2022-11-07 19:42:21 -0500

    m4 up to

--- using_1_to_pred_2.R
+++ using_1_to_pred_2.R
@@ -20,3 +20,12 @@ cv.glm(data = clean, glmfit = lm2)$delta[1]
 
 predictions$pred_m2 = predict(lm2, predictions)
 
+lm3 = glm(R_moment_3 ~ pred_m1*Fr*Re, data = predictions)
+cv.glm(data = predictions, glmfit = lm3)$delta[1]
+
+predictions$pred_m3 = predict(lm3, predictions)
+
+lm4 = glm(R_moment_4 ~ log(St)*Fr*Re, data = predictions)
+cv.glm(data = predictions, glmfit = lm4)$delta[1]
+
+predictions$pred_m4 = predict(lm4, predictions)
